e7b553b524f625e843ed38c3166d3f6aafa14dc0
Ronald22 <ronald22castro@gmail.com>
2018-03-03 17:46:06 -0500

    version 1.3

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.webPort = process.env.PORT || 3000;
 exports.httpMsgsFormat = "JSON";
 
 exports.dbConfig = {
-	host: "localhost",
+	host: "186.70.166.100",
 	user: "root",
 	password: "escuelataller",
 	database: "EscuelaTaller",
